7011f9a51b6285f1260b81e61f6e2cec6be55e05
“Olivia <ttivael@byu.edu>
2024-11-22 19:26:07 -0700

    fixed mongo to have startup database too

--- startupReact/service/database.js
+++ startupReact/service/database.js
@@ -5,7 +5,7 @@ const config = require('./dbConfig.json');
 
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
 const client = new MongoClient(url, {tls:true, serverSelectionTimeoutMS:3000, autoSelectFamily: false,});
-const db = client.db('simon');
+const db = client.db('startup');
 const userCollection = db.collection('user');
 const scoreCollection = db.collection('score');
 
